71fe5d0935e56dc41d3e88642b283ac27e536ac0
Gabriela da Rosa Neto <gabriela.netor94@gmail.com>
2020-08-03 10:40:02 -0300

    Reusable Form e FormField

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,92 +1,104 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
-  const [categorias, setCategorias] = useState(['Teste']);
-  
   const valoresIniciais = {
-    nome: 'Categoria Inicial',
-    descricao: 'Descrição inicial',
-    cor: '#000',
+    nome: '',
+    descricao: '',
+    cor: '',
   }
-  //const [nomeDaCategoria, setNomeDaCategoria] = useState(valoresIniciais);
+  const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
-  function setValue(chave, valor) {
 
-    // chave: nome, descriçao, ....
+  function setValue(chave, valor) {
+    // chave: nome, descricao, bla, bli
     setValues({
       ...values,
-      [chave]: valor,  //pega os valores da chave e define o valor pra ela [essa chave é um valor dinamico] //chave: nome | nome: 'valor'
+      [chave]: valor, // nome: 'valor'
     })
   }
 
   function handleChange(infosDoEvento) {
-    // setNomeDaCategoria(infosDoEvento.target.value);
     setValue(
-      infosDoEvento.target.getAttribute('name'), 
+      infosDoEvento.target.getAttribute('name'),
       infosDoEvento.target.value
-    ); 
+    );
   }
 
   return (
     <PageDefault>
       <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form onSubmit={ function handleSubmit(infosDoEvento) {
+      <form onSubmit={function handleSubmit(infosDoEvento) {
           infosDoEvento.preventDefault();
           setCategorias([
             ...categorias,
-            values  //nomeDaCategoria
+            values
           ]);
+
+          setValues(valoresIniciais)
       }}>
 
-        <div>
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-            value={values.nome} //value={nomeDaCategoria} //value={values['nome']} 
-            name="nome"
-            onChange={ handleChange }
-          />
-        </label>
-        </div>
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
 
-        <div>
-        <label>
-          Descrição: 
-          <textarea
-            type="text"
-            value={values.descricao} //value={nomeDaCategoria}
-            name="descricao"
-            onChange={ handleChange }
-          />
-        </label>
+        <FormField
+          label="Descrição:"
+          type="value"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              name="descricao"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
-        <label>
-          Cor: 
-          <input
-            type="color"
-            value={values.cor}  //value={nomeDaCategoria}
-            name="cor"
-            onChange={ handleChange }
-          />
-        </label>
-        </div>
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={values.cor}
+              name="cor"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
         <button>
           Cadastrar
         </button>
-        
       </form>
+      
 
       <ul>
-        {categorias.map( (categoria, indice) => {
+        {categorias.map((categoria, indice) => {
           return (
             <li key={`${categoria}${indice}`}>
-              {categoria}
+              {categoria.nome}
             </li>
           )
         })}
