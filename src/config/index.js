eca28d97f9c1930c3b486a105bad1210036a99be
Gabriela da Rosa Neto <gabriela.netor94@gmail.com>
2020-08-05 13:31:41 -0300

    the last

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://https://gbflix.vercel.app/';
+  : 'https://gbflix.vercel.app/';
 
 export default {
   URL_BACKEND_TOP,
